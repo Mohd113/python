acaeba08b13b8c5072e4aab5989487edcaf8d418
Mohd113 <mad@unreal.cafe>
2017-04-05 22:22:33 +0300

    Delivery Service

---
+++ delivery.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    ifile = open("C:\\Users\\Chiefan\\Documents\\delivery1.txt", "r")
+    fline = int(ifile.readline())
+    count = 1
+    for i in range(0, fline):
+        cline = int(ifile.readline())
+        time = 0
+        for i in range(0, cline):
+            time += int(ifile.readline())
+        time *= 2
+        print str(count) + ". " + str(time)
+        count += 1
