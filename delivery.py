db6e0191c6238de42490d73828f8e793e5b60ea5
Mohd113 <mad@unreal.cafe>
2017-04-05 22:37:22 +0300

    Delivery Service

--- delivery.py
+++ delivery.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     for i in range(0, fline):
         cline = int(ifile.readline())
         time = 0
-        for i in range(0, cline):
+        for k in range(0, cline):
             time += int(ifile.readline())
         time *= 2
-        print str(count) + ". " + str(time)
-        count += 1
+        print str(i+1) + ". " + str(time)
